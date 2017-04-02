ad181ed8853bbed0f5f9f3e70b352f749f4b475b
aksshah <akshil12@yahoo.co.in>
2017-04-02 08:32:18 +0000

    secondcommit

--- app.js
+++ app.js
@@ -35,6 +35,15 @@ function clear()
 
 
 
-// Currently, it only converts user input currency to static currency which is CAD.
+// Currently, it only converts user input currency to one static currency which is CAD.
 // I tried various things but I'm not able to use my variable "target" to access the 'rates' object from json
-// ex. (data.rates.target) 
+// ex. ((data.rates.target)*value) 
+// I could have used if else condition for every specific currency. eg. if(userinput=="GBP") { ..... (data.rates.GBP)*value....}
+// But it doesn't seem proper to use that many if else. So I've kept it as it is.
+
+
+
+// Please let me know a way to access a property of an object using a variable.
+
+
+
